721fe6411b766350b8332425213b67174d3fc1ad
海野雅義 <isoyasam@unnomasayoshi-no-Mac-mini.local>
2017-01-18 16:34:25 +0900

    URLを本番環境へ

--- assets/public/script/oidc_setting.js
+++ assets/public/script/oidc_setting.js
@@ -4,7 +4,7 @@ var oidc = new OidcController({
         "client_id": "rp-skeleton",
         "response_type": "code",
         "scope": "openid offline",
-        "redirect_uri": "http://skdemo.aoidn.com/callback.html",
+        "redirect_uri": "http://skdemo.aoidn.com:8080/callback.html",
         "authorization_endpoint": "http://skdemo.aoidn.com:1234/oidc/auth",
         "token_session_endpoint": "http://skdemo.aoidn.com:1234/oidc/token",
         "state": "some-random-state-foobar",
